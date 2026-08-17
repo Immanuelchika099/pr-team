36420bc3341b01155b747c8d7ec629d21bad1cbb
Iman <okono3672@gmail.com>
2026-08-17 22:45:31 +0100

    Update Marquee.jsx

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -1,31 +1,27 @@
-import React from 'react';
+import React from "react";
 
+function Marquee() {
+  const items = [
+    "PUBLIC RELATIONS",
+    "SOCIAL MEDIA",
+    "CONTENT CREATION",
+    "BRAND STRATEGY",
+    "CAMPAIGNS",
+    "INFLUENCER ACTIVATION",
+  ];
 
-function Marquee(){
-    const items=
-    [
-        'PUBLIC RELATIONS',
-        'SOCIAL MEDIA',
-        'CONTENT CREATION',
-        'BRAND STRATEGY',
-        'CAMPAIGNS',
-        'INFLUENCER ACTIVATION'
-    ];
-    
-    return (
+  return (
+    <div className="marquee">
+      <div className="marquee-track">
+        {[...items, ...items, ...items, ...items].map((item, i) => (
+          <div key={i} className="marquee-item">
+            <span className="marquee-text">{item}</span>
+            <span className="marquee-dot">•</span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
 
-        <div className="c015 c253 c200 c174 c033 c024">
-            <div className="c010 c283 c046 c099">
-                {[...items,...items,...items,...items].map((item,i)=>
-                
-                <div key={i}className="c046 c099 c167">
-                    <span className="c052 c232 c219 c268 c273">{item}</span>
-                    <span className="c164 c256 c259">•</span>
-                </div>)}
-            </div>
-        </div>
-    )
-    }
-
-
-export default Marquee
+export default Marquee;
